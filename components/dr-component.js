03b334708938893cc0543a936952e0ab6acaee79
vyou <vyou@umich.edu>
2024-07-31 22:33:00 -0400

    setting up svg for d3

--- components/dr-component.js
+++ components/dr-component.js
@@ -6,15 +6,20 @@ class DRComponent extends D3Component {
 
     initialize(node, props) {
         this.width = node.getBoundingClientRect().width;
-        this.height = window.innerHeight;
+        this.height = this.width;
+        //this.height = window.innerHeight;
 
         const svg = this.svg = d3.select(node).append('svg').attr('class', 'canvas');
         svg.attr('viewBox', `0 0 ${this.width} ${this.height}`)
-        .style('width', '100%')
-        .style('height', 'auto')
-        .style('overflow', 'visible')
-        .style('cursor', 'crosshair')
-        .style('box-shadow', '0px 0px 10000px transparent')
+            .style('width', '100%')
+            .style('height', 'auto')
+            .style('overflow', 'visible')
+            .style('cursor', 'crosshair')
+
+        svg.on('click', function() {
+            console.log('clicked');
+        })
+
     }
 
 }
