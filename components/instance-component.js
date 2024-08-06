c0f67e4babd4d5db94507ddb6d572fcadf20f1f4
vyou <vyou@umich.edu>
2024-08-06 03:10:06 -0400

    working on instance component

--- components/instance-component.js
+++ components/instance-component.js
@@ -4,10 +4,10 @@ const d3 = require('d3');
 
 class InstanceComponent extends D3Component {
     initialize(node, props) {
-        const container = d3.select(node).style('position', 'relative').style('width', '300px').style('height', '240px');
+        const container = d3.select(node).style('position', 'relative').style('width', '500px').style('height', '240px');
 
         // Define dimensions
-        const width = 300;
+        const width = 500;
         const height = 240;
         const imageSize = 100;
         const lineLength = 150; // Make the horizontal parts longer
@@ -48,8 +48,8 @@ class InstanceComponent extends D3Component {
 
         // Append moving dotted lines
         const lineData = [
-            { x: imageSize, y: (height / 2) - imageSize - 10 + imageSize / 2, direction: 'down' }, // Adjusted
-            { x: imageSize, y: (height / 2) + 30 + imageSize / 2, direction: 'up' } // Adjusted
+            { x: imageSize, y: (height / 2) - imageSize - 20 + imageSize / 2, direction: 'down' }, // Adjusted
+            { x: imageSize, y: (height / 2) + 20 + imageSize / 2, direction: 'up' } // Adjusted
         ];
 
         lineData.forEach((line, index) => {
@@ -71,10 +71,21 @@ class InstanceComponent extends D3Component {
                 .style('animation', 'dash 0.5s linear infinite'); // Make the animation faster
             
             if (index === 1) {
+
+                // Append another horizontal dotted line coming out of the plus sign
+                svg.append('path')
+                    .attr('d', `M${line.x + lineLength},${height / 2} H${line.x + lineLength + lineLength}`)
+                    .attr('stroke', 'grey')
+                    .attr('stroke-width', 4)
+                    .attr('stroke-dasharray', '5,5')
+                    .attr('fill', 'none')
+                    .attr('class', 'dotted-line')
+                    .style('animation', 'dash 0.5s linear infinite');
+
                 // Append a larger circle with a plus sign at the meeting point
                 svg.append('circle')
                     .attr('cx', line.x + lineLength)
-                    .attr('cy', height / 2 + 10)
+                    .attr('cy', height / 2)
                     .attr('r', 20) // Make the circle larger
                     .attr('fill', '#7f609e') // Purple color
                     .attr('stroke', '#5A2D7A') // Darker purple outline
@@ -82,21 +93,34 @@ class InstanceComponent extends D3Component {
 
                 svg.append('line')
                     .attr('x1', line.x + lineLength - 5)
-                    .attr('y1', height / 2 + 10)
+                    .attr('y1', height / 2)
                     .attr('x2', line.x + lineLength + 5)
-                    .attr('y2', height / 2 + 10)
+                    .attr('y2', height / 2)
                     .attr('stroke', 'white') // White color for the plus sign
                     .attr('stroke-width', 2);
 
                 svg.append('line')
                     .attr('x1', line.x + lineLength)
-                    .attr('y1', height / 2 + 10 - 5)
+                    .attr('y1', height / 2 - 5)
                     .attr('x2', line.x + lineLength)
-                    .attr('y2', height / 2 + 10 + 5)
+                    .attr('y2', height / 2 + 5)
                     .attr('stroke', 'white') // White color for the plus sign
                     .attr('stroke-width', 2);
             }
         });
+
+        // Append another image at the end of the new horizontal line
+        container.append('img')
+            .attr('src', "https://raw.githubusercontent.com/yyou22/VISxAI24_imagebase/main/img_data/0/img.png")
+            .attr('width', imageSize)
+            .attr('height', imageSize)
+            .style('position', 'absolute')
+            .style('left', `${imageSize + lineLength * 2 - imageSize / 2}px`) // Adjusted for the middle point
+            .style('top', `${(height / 2) - imageSize / 2 - 10}px`)
+            .style('border-radius', '10px')
+            .style('box-shadow', '0px 3px 2px #27082a47')
+            .style('border', '10px solid #a5a4a3')
+            .style('outline', '2px solid #505050'); // Add a thin darker gray outline
 
         // Add the CSS animation style
         const style = document.createElement('style');
