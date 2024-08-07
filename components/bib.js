dd7389d74347889fcca7567349bf1ffef5e4b791
vyou <vyou@umich.edu>
2024-08-06 22:07:38 -0400

    finished proofreading the second step

--- components/bib.js
+++ components/bib.js
@@ -1,9 +1,22 @@
-let bibliography = `@article{goodfellow2014explaining,
-title={Explaining and harnessing adversarial examples},
-author={Goodfellow, Ian J and Shlens, Jonathon and Szegedy, Christian},
-journal={arXiv preprint arXiv:1412.6572},
-year={2014},
-DOI={10.48550/arXiv.1412.6572}
-}`;
+let bibliography = "@article{goodfellow2014explaining,\
+title={Explaining and harnessing adversarial examples},\
+author={Goodfellow, Ian J and Shlens, Jonathon and Szegedy, Christian},\
+journal={arXiv preprint arXiv:1412.6572},\
+year={2014},\
+DOI={10.48550/arXiv.1412.6572}\
+}\
+@article{krizhevsky2009learning,\
+title={Learning Multiple Layers of Features from Tiny Images},\
+author={Krizhevsky, A},\
+journal={Master's thesis, University of Toronto},\
+year={2009}\
+}\
+@inproceedings{he2016deep,\
+title={Deep residual learning for image recognition},\
+author={He, Kaiming and Zhang, Xiangyu and Ren, Shaoqing and Sun, Jian},\
+booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},\
+pages={770--778},\
+year={2016}\
+}";
 
 export default bibliography;
