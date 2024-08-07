3c89df53e6ec4449ec3732b3ccfb613664bfa14c
vyou <vyou@umich.edu>
2024-08-06 22:38:48 -0400

    finished proofreading the AT step

--- components/bib.js
+++ components/bib.js
@@ -14,17 +14,31 @@ year={2009}\
 @inproceedings{he2016deep,\
 title={Deep residual learning for image recognition},\
 author={He, Kaiming and Zhang, Xiangyu and Ren, Shaoqing and Sun, Jian},\
-booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},\
+journal={Proceedings of the IEEE conference on computer vision and pattern recognition},\
 pages={770--778},\
 year={2016}\
 }\
 @article{van2008visualizing,\
-title={Visualizing data using t-SNE.},\
+title={Visualizing data using t-SNE},\
 author={Van der Maaten, Laurens and Hinton, Geoffrey},\
 journal={Journal of machine learning research},\
 volume={9},\
 number={11},\
 year={2008}\
+}\
+@article{madry2017towards,\
+title={Towards deep learning models resistant to adversarial attacks},\
+author={Madry, Aleksander and Makelov, Aleksandar and Schmidt, Ludwig and Tsipras, Dimitris and Vladu, Adrian},\
+journal={arXiv preprint arXiv:1706.06083},\
+year={2017}\
+}\
+@inproceedings{zhang2019theoretically,\
+title={Theoretically principled trade-off between robustness and accuracy},\
+author={Zhang, Hongyang and Yu, Yaodong and Jiao, Jiantao and Xing, Eric and El Ghaoui, Laurent and Jordan, Michael},\
+journal={International conference on machine learning},\
+pages={7472--7482},\
+year={2019},\
+organization={PMLR}\
 }";
 
 export default bibliography;
